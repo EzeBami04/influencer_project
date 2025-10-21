855b18589b4b56f6587c5316c86b56121200a41f
Ezekiel omobamidele <140164232+EzeBami04@users.noreply.github.com>
2025-10-21 05:12:09 +0100

    Change environment variable for database password

--- insta.py
+++ insta.py
@@ -359,7 +359,7 @@ if __name__ == "__main__":
         port=os.getenv("DB_PORT"),
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USERNAME"),
-        password=os.getenv("DB_PASS"),
+        password=os.getenv("DB_PASSWORD"),
         sslmode="require")
 
         query = "SELECT instagram_username FROM username_search WHERE instagram_username IS NOT NULL;"
